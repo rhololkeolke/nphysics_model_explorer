822d83ff34378ff18fdd773d3e0d2f45d4025094
Devin Schwab <digidevin@gmail.com>
2019-04-29 23:39:39 -0400

    :sparkles: Basic model-explorer binary setup
    Opens specified model file and creates a world

--- model-explorer/src/main.rs
+++ model-explorer/src/main.rs
@@ -1,4 +1,11 @@
-use std::path::PathBuf;
+use mjcf_parser::MJCFModelDesc;
+use nalgebra as na;
+use nphysics3d::world::World;
+#[allow(unused_imports)]
+use slog::{debug, error, info, trace, warn};
+use slog::{o, Drain};
+use std::fs;
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
 fn parse_log_level(level: &str) -> Result<slog::Level, String> {
@@ -36,8 +43,44 @@ struct Args {
     log_level: slog::Level,
 }
 
+fn make_logger(level: slog::Level, model_file: &Path) -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = drain.filter_level(level).fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    slog::Logger::root(
+        drain,
+        o!("model_file" => model_file.to_string_lossy().to_string(),
+            "place" =>
+           slog::FnValue(move |info| {
+                       format!("{}:{} {}",
+                               info.file(),
+                               info.line(),
+                               info.module(),
+                               )
+        })),
+    )
+}
+
 fn main() {
     let args = Args::from_args();
 
-    println!("args: {:#?}", args);
+    let logger = make_logger(args.log_level, &args.model_file);
+    mjcf_parser::set_root_logger(logger.clone());
+
+    let model_xml: String = fs::read_to_string(args.model_file).expect("Failed to read model file");
+
+    let mut model_desc =
+        MJCFModelDesc::<f32>::parse_xml_string(&model_xml).expect("Failed to parse model file xml");
+
+    // TODO(dschwab): get gravity from model desc
+    let mut world = World::new();
+    world.set_gravity(na::Vector3::z() * -9.91);
+
+    // build model desc
+    model_desc.build(&mut world);
+
+    // run this to force a flush of logs
+    mjcf_parser::drop_root_logger();
 }
